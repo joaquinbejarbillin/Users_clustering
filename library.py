d272ca93b9b4424ef8f1529f196b3650059c1086
joaquin Bejar Garcia <joaquin.bejar@billin.eu>
2018-09-03 16:12:40 +0200

    Changes in class DB

--- library.py
+++ library.py
@@ -25,28 +25,49 @@ class DB():
 
     def __init__(self, user=None, password=None, host=None, db=None):
 
-        #self.user = user
-        #self.password = password
-        #self.host = host
-        #self.db = db
-        self.engine = create_engine('postgresql://{}:{}@{}:5432/{}'.format(user, password,host,db))
+        __host = 'prod-pentaho.cxfaihg8elfv.eu-west-1.rds.amazonaws.com'
+        __db = 'billin_prod'
+        __user = 'billin'
+        __password = 'ThisIsTheRiverOfTheNight'
 
+        if user is None:
+            self.user = __user
+        else:
+            self.user = user
 
-    def gettable(self, db):
-        #contacts = pd.read_sql_query('select * from "contacts"',con=engine)
-        #users = pd.read_sql_query('select * from "users"',con=engine)
-        #sessions = pd.read_sql_query('select * from "sessions"',con=engine)
-        #premiums = pd.read_sql_query('select * from "premiums"',con=engine)
-        #gocardlesses = pd.read_sql_query('select * from "gocardlesses"',con=engine)
-        #campaigns = pd.read_sql_query('select * from "campaigns"',con=engine)
-        #campaign_details = pd.read_sql_query('select * from "campaign-details"',con=engine)
-        #businessesUsers = pd.read_sql_query('select * from "businessesUsers"',con=engine)
-        #businesses = pd.read_sql_query('select * from "businesses"',con=engine)
-        #businessConstacts = pd.read_sql_query('select * from "businessContacts"',con=engine)
-        #bankAccounts = pd.read_sql_query('select * from "bankAccounts"',con=engine)
-        #addresses = pd.read_sql_query('select * from "addresses"',con=engine)
+        if password is None:
+            self.password = __password
+        else:
+            self.password = password
 
-        return pd.read_sql_query('select * from "{}"'.format(db),con=self.engine)
+        if host is None:
+            self.host = __host
+        else:
+            self.host = host
+
+        if db is None:
+            self.db = __db
+        else:
+            self.db = db
+
+        self.engine = create_engine(
+            'postgresql://{}:{}@{}:5432/{}'.format(self.user, self.password, self.host, self.db))
+
+    def gettable(self, table):
+        # contacts = pd.read_sql_query('select * from "contacts"',con=engine)
+        # users = pd.read_sql_query('select * from "users"',con=engine)
+        # sessions = pd.read_sql_query('select * from "sessions"',con=engine)
+        # premiums = pd.read_sql_query('select * from "premiums"',con=engine)
+        # gocardlesses = pd.read_sql_query('select * from "gocardlesses"',con=engine)
+        # campaigns = pd.read_sql_query('select * from "campaigns"',con=engine)
+        # campaign_details = pd.read_sql_query('select * from "campaign-details"',con=engine)
+        # businessesUsers = pd.read_sql_query('select * from "businessesUsers"',con=engine)
+        # businesses = pd.read_sql_query('select * from "businesses"',con=engine)
+        # businessConstacts = pd.read_sql_query('select * from "businessContacts"',con=engine)
+        # bankAccounts = pd.read_sql_query('select * from "bankAccounts"',con=engine)
+        # addresses = pd.read_sql_query('select * from "addresses"',con=engine)
+
+        return pd.read_sql_query('select * from "{}"'.format(table), con=self.engine)
 
 
 def ScatterMatrix(df):
